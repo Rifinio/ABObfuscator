b54d56a2e66ac981ebfea0ef177345f51004366b
Adil Bougamza <adil.bougamza@gmail.com>
2018-09-11 16:33:43 +0200

    Show an alert using the AlertPresenter helper class

--- ABObfusctaedAlerts/ViewController.swift
+++ ABObfusctaedAlerts/ViewController.swift
@@ -10,17 +10,42 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var alertButton: UIButton!
+
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        setupView()
+        setupContraints()
+    }
+
+    func setupView() {
         view.backgroundColor = .white
-        
+
+        alertButton = UIButton(type: .system)
+        view.addSubview(alertButton)
+        alertButton.setTitle("Show Alerts", for: .normal)
+        alertButton.addTarget(self, action: #selector(showAlert), for: .touchUpInside)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func setupContraints() {
+        alertButton.translatesAutoresizingMaskIntoConstraints = false
+        alertButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20).isActive = true
+        alertButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20).isActive = true
+        alertButton.heightAnchor.constraint(equalToConstant: 44).isActive = true
+        alertButton.topAnchor.constraint(equalTo: view.topAnchor, constant: 100).isActive = true
     }
 
+    @objc func showAlert() {
+        let action1 = UIAlertAction(title: "Default", style: .default) { _ in
+            print("You've pressed default");
+        }
+        let action2 = UIAlertAction(title: "Destructive", style: .destructive) { _ in
+            print("You've pressed the destructive");
+        }
 
+        let alertPresenter = AlertPresenter()
+        alertPresenter.showAlert(from: self, title: "title", message: "message", actions: [action1, action2], completion: nil)
+    }
 }
 
